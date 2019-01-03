7e503eaa6b4ce899b54acd08df475a8db0d4daa6
Luiz Bosso <lfernando.bosso@gmail.com>
2019-01-03 11:06:36 -0200

    Implements points method on player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,7 @@
 class Player < ApplicationRecord
+  has_many :point_entries, dependent: :destroy
+
+  def points
+    point_entries.sum(&:value)
+  end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#points' do
+    subject { player.points }
+
+    context 'with no points' do
+      let(:player) { create :player }
+
+      it { is_expected.to be_zero }
+    end
+
+    context 'with points' do
+      let(:player) { create(:player, :with_points) }
+
+      it { is_expected.to eq(player.point_entries.sum(&:value)) }
+    end
+  end
 end
